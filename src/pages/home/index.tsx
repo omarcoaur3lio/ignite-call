e17927c52e760e9c5cf5d386d438fbf548e90116
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-08-28 21:16:45 -0300

    :construction: WIP: Layout da Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from '@ignite-js-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando a aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,1 @@
-import { Heading, Text } from '@ignite-js-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1">Hello Next.js</Heading>
-      <Text>Hello World</Text>
-    </>
-  )
-}
+export { default } from './home'
