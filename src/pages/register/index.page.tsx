ed5416eab3df8706cc58c33cd3387493e09f38df
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-09-09 23:00:26 -0300

    :sparkles: feat: Adicionar rota para cadastro de usuário

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -12,6 +12,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -46,7 +47,16 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  function handleRegister() {}
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <Container>
