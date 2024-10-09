d45ee8f33e93e117071ebeb271f857830108ac18
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-10-09 17:49:28 -0300

    :sparkles: feat: Adicionar tratamento para erro no login com o google

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-js-ui/react'
+import { Box, styled, Text } from '@ignite-js-ui/react'
 
 export const ConnectBox = styled(Box, {
   marginTop: '$6',
@@ -10,9 +10,14 @@ export const ConnectItem = styled('div', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
-  marginBottom: '$2',
+  marginBottom: '$4',
 
   border: '1px solid $gray600',
   borderRadius: '$md',
   padding: '$4 $6',
+})
+
+export const AuthError = styled(Text, {
+  color: '#F75A68',
+  marginBottom: '$4',
 })
