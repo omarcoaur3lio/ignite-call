bf3db3dccd4d457651572b79db6ebeae0c0ff527
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-11-26 22:44:06 -0300

    :sparkles: Persistir intervalos no banco de dados

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,7 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
 
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
@@ -16,7 +27,24 @@ export default async function handler(
     buildNextAuthOptions(request, response),
   )
 
-  return response.json({
-    session,
-  })
+  if (!session) {
+    return response.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(request.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return response.status(201).end()
 }
